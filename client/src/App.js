4135ddffc8349fbb3cbc7f80548224e9da9cd180
JasaJovan99 <jovan.jasa@gmail.com>
2021-03-03 11:09:56 +0100

    figured out how to get data from the backend server and display on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+
+const Test = () => {
+  const [answer, setAnwser] = useState();
+
+  const getAnswer = async () => {
+    const res = await fetch("/api");
+    const answer = await res.json();
+    setAnwser(answer);
+  };
+
+  useEffect(() => {
+    getAnswer();
+  }, []);
+
+  return <div>{ JSON.stringify(answer) }</div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>alkjsjdjaksjd</p>
+      <Test />
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT ||8080;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api', (req, res) => {
+  res.json({ dela: 'askjdhaskjd' })
+})
 
 app.use(morgan('tiny'));
 
